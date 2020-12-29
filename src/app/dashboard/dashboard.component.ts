f282369977ef0535a4cfbf6b603140d1ff840a46
unknown <unenes.viejos@gmail.com>
2020-12-29 10:27:22 -0600

    Styles en mi componente

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,8 @@ import {Component} from '@angular/core';
 @Component({
 
     selector:'app-dashboard',
-    template: '<h1>Hola mundo!</h1>'
+    template: '<h1>Hola mundo!</h1>',
+    styles: ['h1{ color: red;}']
 
 })
 
