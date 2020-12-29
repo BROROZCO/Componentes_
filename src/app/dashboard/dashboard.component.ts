bc53218836e3d6c11382432ef0443d68a651bde7
unknown <unenes.viejos@gmail.com>
2020-12-29 10:01:12 -0600

    Creacion de los 3 elementos de un comonente en el TS de el directorio dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+// toda clase tiene el decorador @
+// un componente tiene tres partes el importacion, decorador y la clase
+
+import {Component} from '@angular/core';
+
+@Component({
+
+})
+
+export class DashboardComponent {
+
+}
