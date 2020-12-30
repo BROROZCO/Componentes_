fb96eb911d98951e815c3d64a92e211244829bb4
unknown <unenes.viejos@gmail.com>
2020-12-29 20:07:05 -0600

    Creacion del componente tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule
